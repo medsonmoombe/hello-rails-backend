01de7e3d02968c09a68f54bc68adf63b7a443119
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-21 10:37:38 +0200

    modified the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
       resources :greetings, only: [:index] 
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root 'api/v1/greetings#index'
 end
