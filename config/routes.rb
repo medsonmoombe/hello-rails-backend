72ce9c5d6c3db8ce1ae2f3242d65ea72b0a03d21
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-21 10:35:36 +0200

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      resources :greetings, only: [:index] 
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'api/v1/greetings#index'
 end
